46d4eb3efd5f2c60dce9e0cca1e03fe837549db4
yuu-eguci <yuu.eguci@gmail.com>
2020-12-25 20:58:55 +0900

    🐛 tutorial -> RESTaurant
    ああ、 import で tutorial を使っていたのね。

--- RESTaurant/quickstart/views.py
+++ RESTaurant/quickstart/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from RESTaurant.quickstart.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
--- RESTaurant/urls.py
+++ RESTaurant/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers
-from tutorial.quickstart import views
+from RESTaurant.quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
