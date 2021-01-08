611575b1bf051f4b42d63d02148808e20038e5b1
yuu-eguci <yuu.eguci@gmail.com>
2021-01-09 02:31:04 +0900

    ✨ Add my_customized_server_error for 500

--- RESTaurant/quickstart/views.py
+++ RESTaurant/quickstart/views.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from RESTaurant.quickstart.serializers import UserSerializer, GroupSerializer
+from django.views.decorators.csrf import requires_csrf_token
+from django.http import (
+    HttpResponseServerError,
+)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +24,18 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+@requires_csrf_token
+def my_customized_server_error(request, template_name='500.html'):
+
+    # NOTE: print が App Service で機能するかどうか確かめるために print しています。
+    import traceback
+    print(traceback.format_exc())
+    # return HttpResponseServerError('<h1>Server Error (500)</h1>')
+
+    # DEBUG = True と同様の画面を出します。
+    import sys
+    from django.views import debug
+    error_html = debug.technical_500_response(request, *sys.exc_info()).content
+    return HttpResponseServerError(error_html)
--- RESTaurant/urls.py
+++ RESTaurant/urls.py
@@ -27,3 +27,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+handler500 = views.my_customized_server_error
