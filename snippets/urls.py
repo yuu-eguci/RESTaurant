5b2718f629295dee587a7ba1b2393ec77e4dd660
yuu-eguci <yuu.eguci@gmail.com>
2021-01-10 23:49:02 +0900

    ✨ 【T3】 Rewriting our API using class-based views

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,8 +3,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    # NOTE: view を def で書くなら
+    #       path('snippets/', views.snippet_list)
+    #       こんな感じだけど、 class で定義した場合こうなる。
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -12,20 +12,28 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+# NOTE: 【T3】で追加。
+from rest_framework.views import APIView
+from django.http import Http404
 
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
+
+# NOTE: @api_view(['GET', 'POST']) 付きの def で書いてしまうと
+#       if request.method == 'GET':
+#       elif request.method == 'POST':
+#       でリクエスト分けをすることになってしまいます。 class のほうが洗練されています。
+class SnippetList(APIView):
     """
-    List all code snippets, or create a new snippet.
+    List all snippets, or create a new snippet.
     """
-    if request.method == 'GET':
+
+    def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         # NOTE: JsonResponse(serializer.data, safe=False) よりこっちのほうがいい。
         #       Response で返すことによって web 画面に REST framework の view が出るようになる。
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         # data = JSONParser().parse(request)
         # serializer = SnippetSerializer(data=data)
         # NOTE: 以上の処理を request.data で簡略可。
@@ -37,28 +45,35 @@ def snippet_list(request, format=None):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
+# NOTE: ↑と同じく @api_view(['GET', 'PUT', 'DELETE']) 付きの def では書かない。
+class SnippetDetail(APIView):
     """
-    Retrieve, update or delete a code snippet.
+    Retrieve, update or delete a snippet instance.
     """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        # NOTE: status=404 -> status.HTTP_404_NOT_FOUND こっちのほうがいい。
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            # NOTE: status=404 -> status.HTTP_404_NOT_FOUND こっちのほうがいい。
+            #       が、 Response(status=status.HTTP_404_NOT_FOUND) よりも
+            #       raise Http404 のほうがさらによい。
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
