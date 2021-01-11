e37ba9e013c3e668eb859f35ed16550549e5f726
yuu-eguci <yuu.eguci@gmail.com>
2021-01-11 12:28:47 +0900

    ✨ samplecommand

---
+++ snippets/management/commands/samplecommand.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+
+class Command(BaseCommand):
+    help = 'Displays current time'
+
+    def handle(self, *args, **kwargs):
+        now = timezone.now()
+        iso = now.strftime('%Y-%m-%dT%H:%M:%SZ')
+        tzinfo = now.tzinfo
+        self.stdout.write(f'timezone.now(): {iso}, tzinfo: {tzinfo}')
