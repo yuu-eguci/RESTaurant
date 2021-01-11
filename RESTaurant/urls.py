dd36a64ddf9d8e58e58391d2ff7ec1a108734b05
yuu-eguci <yuu.eguci@gmail.com>
2021-01-11 10:42:10 +0900

    ✨ 【T6】 Using Routers

--- RESTaurant/urls.py
+++ RESTaurant/urls.py
@@ -14,43 +14,53 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
-from rest_framework import routers
-from snippets.views import SnippetViewSet, UserViewSet, api_root, my_customized_server_error
+from rest_framework.routers import DefaultRouter
+from snippets import views
+# from snippets.views import SnippetViewSet, UserViewSet, api_root, my_customized_server_error
 from rest_framework import renderers
 
 # router = routers.DefaultRouter()
 # router.register(r'users', views.UserViewSet)
 # router.register(r'groups', views.GroupViewSet)
 
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
+# Create a router and register our viewsets with it.
+# NOTE: view クラスではなく ViewSet を使うことでこういうやり方ができる。
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# NOTE: ViewSet を使って↑のように書くことで、これが全部省略できる。
+# snippet_list = SnippetViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+# snippet_detail = SnippetViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+# snippet_highlight = SnippetViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+# user_list = UserViewSet.as_view({
+#     'get': 'list'
+# })
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', api_root),
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail'),
+    path('', include(router.urls)),
+    # NOTE: ViewSet を使って↑のように書くことで、これが全部省略できる。
+    # path('', api_root),
+    # path('snippets/', snippet_list, name='snippet-list'),
+    # path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    # path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    # path('users/', user_list, name='user-list'),
+    # path('users/<int:pk>/', user_detail, name='user-detail'),
 ]
 
-handler500 = my_customized_server_error
+handler500 = views.my_customized_server_error
