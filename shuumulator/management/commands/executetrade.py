147b0b7fd21afbb72feb6a2c3c174c78d15f9b81
yuu-eguci <yuu.eguci@gmail.com>
2021-01-11 18:26:33 +0900

    shuumulator ✨ executetrade.py comment program

--- shuumulator/management/commands/executetrade.py
+++ shuumulator/management/commands/executetrade.py
@@ -10,3 +10,22 @@ class Command(BaseCommand):
         iso = now.strftime('%Y-%m-%dT%H:%M:%SZ')
         tzinfo = now.tzinfo
         self.stdout.write(f'timezone.now(): {iso}, tzinfo: {tzinfo}')
+
+        return main()
+
+
+def main():
+
+    # 銘柄の情報を収集します。
+
+    # 買うかどうかを判断します。(2,000円以下とかを考えています。)
+
+    # Stock マスタになければ追加します。(Stock 追加)
+
+    # 現在持っていないものは購入します。(TradingRecord 追加)
+
+    # 現在の勝率を計算し、利確ラインと損切りラインを算出します。
+
+    # ラインを超えている Stock があれば売却します。(TradingRecord 更新)
+
+    return
