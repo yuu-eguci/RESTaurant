a3ea42b9fd3d6c84feed677472aa3bb8e74e770b
yuu-eguci <yuu.eguci@gmail.com>
2021-01-11 09:36:46 +0900

    🔒 【T4】 models.Snippet.save

--- snippets/models.py
+++ snippets/models.py
@@ -5,6 +5,11 @@ from django.db import models
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
+# NOTE: 【T4】で追加。
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
+
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
@@ -23,6 +28,26 @@ class Snippet(models.Model):
         choices=STYLE_CHOICES,
         default='friendly',
         max_length=100)
+
+    # NOTE: save メソッドのために追加。
+    owner = models.ForeignKey(
+        'auth.User',
+        related_name='snippets',
+        on_delete=models.CASCADE)
+    highlighted = models.TextField()
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                  full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['created']
